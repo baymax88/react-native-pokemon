4c9e2baf4f276b337c39cb8978261b8d0986d87f
baymax88 <smartbay1@outlook.com>
2020-05-07 02:46:06 +0800

    detail component init

---
+++ Components/Details.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+const Details = props => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details</Text>
+    </View>);
+};
+export default Details;
